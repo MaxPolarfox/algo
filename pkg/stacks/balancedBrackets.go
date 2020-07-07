32477822f9d06e1c41dd7c6cee88fbf0d7957615
maksim_pesetski <maksim.pesetski@gmail.com>
2020-07-07 19:53:31 -0400

    added GroupAnagrams, LongestPalindromicSubstring, balancedBrackets

---
+++ pkg/stacks/balancedBrackets.go
@@ -0,0 +1,36 @@
+package stacks
+
+var opening = map[rune]bool{
+	'{': true,
+	'[': true,
+	'(': true,
+}
+var closing = map[rune]bool{
+	'}': true,
+	']': true,
+	')': true,}
+var matching = map[rune]rune{
+	'}': '{',
+	']': '[',
+	')': '(',}
+
+func BalancedBrackets(s string) bool {
+	stack := []rune{}
+	for _, char := range s {
+		if opening[char] {
+			stack = append(stack, char)
+			continue
+		}
+		if closing[char] {
+			if len(stack) == 0 {
+				return false
+			}
+			if matching[char] == stack[len(stack) - 1] {
+				stack = stack[0:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
---
+++ pkg/strings/longestPalindromicSubstring.go
@@ -0,0 +1,27 @@
+package strings
+
+func LongestPalindromicSubstring(str string) string {
+	longest := ""
+
+	for i:=0; i< len(str); i++ {
+		current := string(str[i])
+		l := i-1
+		r := i+1
+
+		for r < len(str) && str[i] == str[r]  {
+			current += string(str[r])
+			r++
+		}
+
+		for l >= 0 && r < len(str) && str[l] == str[r] {
+			current = string(str[l]) + current + string(str[r])
+			l--
+			r++
+		}
+
+		if len(current) > len(longest) {
+			longest = current
+		}
+	}
+	return longest
+}
