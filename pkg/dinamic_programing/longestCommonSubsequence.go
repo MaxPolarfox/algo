f6d136927c95cd1a9c7e89b5014259ee08e3ff43
maksim_pesetski <maksim.pesetski@gmail.com>
2020-07-29 19:12:52 -0400

    fmt

--- pkg/dinamic_programing/longestCommonSubsequence.go
+++ pkg/dinamic_programing/longestCommonSubsequence.go
@@ -1,13 +1,11 @@
 package dinamic_programing
 
 func LongestCommonSubsequence(s1 string, s2 string) string {
-	lengths := make([][]int, len(s2) + 1)
-	for i := range lengths {
-		lengths[i] = make([]int, len(s1)+1)
+	lengths := make([][]int, len(s2)+1)
+	for i := range lengths {lengths[i] = make([]int, len(s1)+1)
 	}
-
-	for i := 1; i < len(s2) +1; i++ {
-		for j := 1; j < len(s1) +1 ; j++ {
+	for i := 1; i < len(s2)+1; i++ {
+		for j := 1; j < len(s1)+1; j++ {
 			if s2[i-1] == s1[j-1] {
 				lengths[i][j] = lengths[i-1][j-1] + 1
 			} else {
@@ -26,7 +24,7 @@ func buildLongestCommonSubsequence(lengths [][]int, s1 string) string {
 	for i != 0 && j != 0 {
 		if lengths[i][j] == lengths[i-1][j] {
 			i--
-		} else if lengths[i][j] == lengths[i][j - 1] {
+		} else if lengths[i][j] == lengths[i][j-1] {
 			j--
 		} else {
 			sequence = append([]byte{s1[j-1]}, sequence...)
